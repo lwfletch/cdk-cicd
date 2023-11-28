05746567a4719b237d64498b96d737baf8c9e4b5
Trey Fletcher <treyfletcher612@gmail.com>
2023-11-28 09:08:42 -0500

    created empty pipeline stack

--- lib/cdk-cicd-stack.ts
+++ lib/cdk-cicd-stack.ts
@@ -1,10 +1,20 @@
 import * as cdk from 'aws-cdk-lib';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
 
 export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    
+    new CodePipeline(this, 'TreysPipeline', {
+      pipelineName: 'TreysPipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('lwfletch/cdk-cicd', 'master'),
+        commands: [
+          'npm ci',
+          'npx cdk synth'
+        ],
+      })
+    })
   }
 }
